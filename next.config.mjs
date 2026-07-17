e9ea5223bd552b4557c338640b97b99d04b3c4f6
selllek <45856406+selllek@users.noreply.github.com>
2026-07-17 15:31:58 -0300

    Update and rename vercel.json to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Aplica o CORS para todas as rotas de API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
+
+export default nextConfig;
